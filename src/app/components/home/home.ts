f0f8d742713bb9993fb278223ef0df7164fee88b
josepereza <josepereza66@gmail.com>
2025-11-16 20:50:58 +0100

    cambiado a aleman3

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Features } from '../features/features';
 import { Footer } from '../footer/footer';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [Features],
+  imports: [Features,RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
